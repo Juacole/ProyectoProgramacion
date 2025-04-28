6ac84ffaf10ceacfc54f8dd48fe8910b19695c61
Juacole <jvuacotunjargmail.com>
2025-04-28 11:06:21 -0500

    Se añaden getters y setters

--- src/Equipamiento.java
+++ src/Equipamiento.java
@@ -9,8 +9,14 @@ public abstract class Equipamiento {
     public Equipamiento() {
         this.nombre = "";
         this.estadisticas = new HashMap<>();
+        this.estadisticas.put("Ataque", 0);
+        this.estadisticas.put("Velocidad", 0);
+        this.estadisticas.put("Magia", 0);
+        this.estadisticas.put("Fe", 0);
+        this.estadisticas.put("Armadura", 0);
+        this.estadisticas.put("ResistenciaMagica", 0);
         this.rareza = "";
-        this.valor_economico = -1;
+        this.valor_economico = 0;
     }
 
     public Equipamiento(HashMap<String, Integer> estadisticas, String rareza, double valor_economico) {
@@ -19,4 +25,59 @@ public abstract class Equipamiento {
         this.rareza = rareza;
         this.valor_economico = valor_economico;
     }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getRareza() {
+        return this.rareza;
+    }
+
+    public double getValorEconomico() {
+        return this.valor_economico;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre.length() > 20) {
+            System.err.println("El nombre no puede tener mas de 20 digitos.");
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    public void setRareza(String rareza) {
+        switch (rareza.toLowerCase().strip()) {
+            case "comun":
+                this.rareza = rareza;
+                break;
+
+            case "raro":
+                this.rareza = rareza;
+                break;
+
+            case "epico":
+                this.rareza = rareza;
+                break;
+
+            case "legendario":
+                this.rareza = rareza;
+                break;
+
+            default:
+                System.err.println("La rareza solo puede ser comun, raro, epico y legendario.");
+                this.nombre = "comun";
+                break;
+        }
+    }
+
+    public void setValorEconomico(double valor_economico) {
+        if (valor_economico < 1) {
+            System.err.println("El valor economico de un equipamiento no puede ser menor que 1 moneda de oro.");
+            this.valor_economico = 1;
+        } else {
+            this.valor_economico = valor_economico;
+        }
+    }
+
 }
